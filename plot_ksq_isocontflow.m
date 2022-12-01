8bd552b003e31b98e0551b04aa1a76d47b2bb159
Hari Krishna Hari Prasad <hari.hariprasad@colorado.edu>
2022-12-01 16:39:08 -0700

    updated se2_toyproblems_case_1.mlx

--- plot_ksq_isocontflow.m
+++ plot_ksq_isocontflow.m
@@ -3,8 +3,9 @@ function plot_ksq_isocontflow(k,d_x,d_y,cs1,cs2)
 % Get the text for the labels
 xtxt = ['$\alpha_' num2str(cs1) '$'];
 ytxt = ['$\alpha_' num2str(cs2) '$'];
-ctitle_txt = ['$k^2_{' num2str(cs1) num2str(cs2) '}$ contours'];
-ftitle_txt = ['iso-$k^2_{' num2str(cs1) num2str(cs2) '}$ flow'];
+% ctitle_txt = ['$k^2_{' num2str(cs1) num2str(cs2) '}$ contours'];
+% ftitle_txt = ['iso-$k^2_{' num2str(cs1) num2str(cs2) '}$ flow'];
+cftitle_txt = ['$k^2_{' num2str(cs1) num2str(cs2) '}$ iso-contours and flow'];
 
 % Unlock the joint limits of the system.
 ank = 1*pi; % symmetrically defined about 0.
@@ -14,8 +15,8 @@ aa = 1; ll = 1;
 dnum = 1001; % set this arbitrarily
 skipV = 50; % number of points to skip when plotting the vector field
 cLvl = 25; % number of countour levels
-lW_contour = (1.2*10)/cLvl; % contour linewidth scales with the number of countour lines
-lW_Vector = (1.2/100)*skipV; % vector linewidth scales with the density of the vector field
+lW_contour = (2.0*10)/cLvl; % contour linewidth scales with the number of countour lines
+lW_Vector = (2.0/100)*skipV; % vector linewidth scales with the density of the vector field
 idxQ = 1:skipV:dnum; % indices to plot the vector field at
 xx = linspace(-ank,ank,dnum);         
 yy = xx;
@@ -40,12 +41,12 @@ subplot(1,2,1);
 contour(ai,aj,ksq_sweep,cLvl,'LineWidth',lW_contour);
 axis equal tight; hold on; view(2);
 colormap(jet);
-colorbar();
+colorbar('TickLabelInterpreter','latex','FontSize',12);
 % line(xx,yy,ksq_M,'Color','k','LineStyle','-.');
 % line(xx,-yy,ksq_m,'Color','k','LineStyle','--');
 % https://www.mathworks.com/matlabcentral/answers/271912-rotate-ylabel-and-keep-centered
 set(get(gca,'YLabel'),'rotation',0,'VerticalAlignment','middle');
-title(ctitle_txt,FontSize=20);
+% title(ctitle_txt,FontSize=20);
 xticks(-pi:pi/2:pi);
 xticklabels({'$-\pi$','$-\frac{\pi}{2}$','$0$','$\frac{\pi}{2}$','$\pi$'});
 yticks(-pi:pi/2:pi);
@@ -61,7 +62,7 @@ quiver(ai(idxQ,idxQ),aj(idxQ,idxQ),u_sweep(idxQ,idxQ),v_sweep(idxQ,idxQ),...
     'LineWidth',lW_Vector,'Color','k');
 axis equal tight; hold on; view(2);
 set(get(gca,'YLabel'),'rotation',0,'VerticalAlignment','middle');
-title(ftitle_txt,FontSize=20);
+% title(ftitle_txt,FontSize=20);
 xticks(-pi:pi/2:pi);
 xticklabels({'$-\pi$','$-\frac{\pi}{2}$','$0$','$\frac{\pi}{2}$','$\pi$'});
 yticks(-pi:pi/2:pi);
@@ -71,5 +72,7 @@ ax.XAxis.FontSize = 15;
 ax.YAxis.FontSize = 15;
 xlabel(xtxt,FontSize=20);
 ylabel(ytxt,FontSize=20);
+
+sgtitle(cftitle_txt,FontSize=20);
 
 end
