fe097e544f5dd45b727d3b1fc83745fbc35feb0b
Hari Krishna Hari Prasad <hari.hariprasad@colorado.edu>
2023-02-07 13:25:02 -0700

    identified the wrong transform in case_1 kinematics

---
+++ Utility Functions/seqSE2transformation.m
@@ -0,0 +1,11 @@
+%seqSE2transformation compute a sequence of SE2 transformations given in a left-to-right stacked input array
+function [out_arr] = seqSE2transformation(in_arr)
+    %   Each column in the array corresponds to the group element operation from left to right. The columns are SE2 vectors, so they need to be converted into their
+    %   corresponding matrix forms to act on each other.
+    
+
+
+    out_arr
+
+end
+
