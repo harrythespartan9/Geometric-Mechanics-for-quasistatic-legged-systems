8fccf4e7a8519a911ed0618b8f3805b56b6d2a4a
Hari Krishna Hari Prasad <hari.hariprasad@colorado.edu>
2023-02-07 21:11:01 -0700

    fixed the case_1 kinematics

--- Utility Functions/seqSE2transformation.m
+++ Utility Functions/seqSE2transformation.m
@@ -1,11 +1,25 @@
 %seqSE2transformation compute a sequence of SE2 transformations given in a left-to-right stacked input array
-function [out_arr] = seqSE2transformation(in_arr)
+function cum_vector = seqSE2transformation(in_arr)
     %   Each column in the array corresponds to the group element operation from left to right. The columns are SE2 vectors, so they need to be converted into their
     %   corresponding matrix forms to act on each other.
+
+    % Initialize SE(2) transform containers
+    cum_angle = sym(0);
+    cum_transform = sym(eye(3));
+
+    % Iterate over each transform in the order of the input array arranged from left to right
+    for i = 1:size(in_arr, 2)
+        
+        % updated the cumulative transform angle
+        cum_angle = cum_angle + in_arr(3,i);
+
+        % update the transform
+        cum_transform = cum_transform*v2M_SE2(in_arr(:,i));
+
+    end
     
-
-
-    out_arr
-
+    % return the vector form of this cumulative transform
+    cum_vector = [cum_transform(1,3), cum_transform(2,3), cum_angle]';
+    
 end
 
---
+++ Utility Functions/v2M_SE2.m
@@ -0,0 +1,10 @@
+%V2M_SE2 converts the vector form of an SE2 group element to its actionable matrix form
+function M = v2M_SE2(v)
+    
+    % Compute the Matrix form directly
+    M = [cos(v(3)), -sin(v(3)), v(1);
+         sin(v(3)),  cos(v(3)), v(2);
+                 0,         0,    1];
+
+end
+
