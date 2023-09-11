8847e66713af9cf4336588a74e89bba58a025f6a
Hari Krishna Hari Prasad <hari.hariprasad@colorado.edu>
2023-09-11 08:10:31 -0600

    updated rigid quad noslip motion planning code
    1) noslip2bgaits.m: removed some unexplained transposing
    2) stitchshapefor2bgait.m-- made sure the subgaits return path reflects the forward path
    Need to test these two changes

--- Utility Functions/stitchshapefor2bgait.m
+++ Utility Functions/stitchshapefor2bgait.m
@@ -6,26 +6,30 @@ function [a1, a2] = stitchshapefor2bgait(before, during, switching, after, a1A, 
             
             temp_before_i = a1A(1)*ones(1, sum(before)+1); temp_before_i = temp_before_i(1:end-1);
             temp_active_i = interp1( 1:numel(a1A), a1A, linspace(1,numel(a1A), sum(during)+1 ), 'spline' ); temp_active_i = temp_active_i(1:end-1);
-            temp_switch_i = linspace( a1A(end), a1A(1), sum(switching)+1 ); temp_switch_i = temp_switch_i(1:end-1);
+            % temp_switch_i = linspace( a1A(end), a1A(1), sum(switching)+1 ); temp_switch_i = temp_switch_i(1:end-1);
+            temp_switch_i = interp1( 1:numel(a1A), fliplr(a1A), linspace(1,numel(a1A), sum(switching)+1 ), 'spline' ); temp_switch_i = temp_switch_i(1:end-1);
             temp_after_i = a1A(1)*ones(1, sum(after)+1); temp_after_i = temp_after_i(1:end-1);
             a1 = [ temp_before_i, temp_active_i, temp_switch_i, temp_after_i ];
 
             temp_before_i = a2A(1)*ones(1, sum(before)+1); temp_before_i = temp_before_i(1:end-1);
             temp_active_i = interp1( 1:numel(a2A), a2A, linspace(1,numel(a2A), sum(during)+1 ), 'spline' ); temp_active_i = temp_active_i(1:end-1);
-            temp_switch_i = linspace( a2A(end), a2A(1), sum(switching)+1 ); temp_switch_i = temp_switch_i(1:end-1);
-            temp_after_i = a2A(1)*ones(1, sum(after & ~switching)+1); temp_after_i = temp_after_i(1:end-1);
+            % temp_switch_i = linspace( a2A(end), a2A(1), sum(switching)+1 ); temp_switch_i = temp_switch_i(1:end-1);
+            temp_switch_i = interp1( 1:numel(a2A), fliplr(a2A), linspace(1,numel(a2A), sum(switching)+1 ), 'spline' ); temp_switch_i = temp_switch_i(1:end-1);
+            temp_after_i = a2A(1)*ones(1, sum(after)+1); temp_after_i = temp_after_i(1:end-1); % after & ~switching
             a2 = [ temp_before_i, temp_active_i, temp_switch_i, temp_after_i ];
 
         case 2
             
             temp_before_j = a1A(end)*ones(1, sum(before)+1); temp_before_j = temp_before_j(1:end-1);
-            temp_switch_j = linspace( a1A(end), a1A(1), sum(switching)+1 ); temp_switch_j = temp_switch_j(1:end-1);
+            % temp_switch_j = linspace( a1A(end), a1A(1), sum(switching)+1 ); temp_switch_j = temp_switch_j(1:end-1);
+            temp_switch_j = interp1( 1:numel(a1A), fliplr(a1A), linspace(1,numel(a1A), sum(switching)+1 ), 'spline' ); temp_switch_j = temp_switch_j(1:end-1);
             temp_active_j = interp1( 1:numel(a1A), a1A, linspace(1,numel(a1A), sum(during)+1 ), 'spline' ); temp_active_j = temp_active_j(1:end-1);
             temp_after_j = a1A(end)*ones(1, sum(after)+1); temp_after_j = temp_after_j(1:end-1);
             a1 = [ temp_before_j, temp_switch_j, temp_active_j, temp_after_j ];
 
             temp_before_j = a2A(end)*ones(1, sum(before)+1); temp_before_j = temp_before_j(1:end-1);
-            temp_switch_j = linspace( a2A(end), a2A(1), sum(switching)+1 ); temp_switch_j = temp_switch_j(1:end-1);
+            % temp_switch_j = linspace( a2A(end), a2A(1), sum(switching)+1 ); temp_switch_j = temp_switch_j(1:end-1);
+            temp_switch_j = interp1( 1:numel(a2A), fliplr(a2A), linspace(1,numel(a2A), sum(switching)+1 ), 'spline' ); temp_switch_j = temp_switch_j(1:end-1);
             temp_active_j = interp1( 1:numel(a2A), a2A, linspace(1,numel(a2A), sum(during)+1 ), 'spline' ); temp_active_j = temp_active_j(1:end-1);
             temp_after_j = a2A(end)*ones(1, sum(after)+1); temp_after_j = temp_after_j(1:end-1);
             a2 = [ temp_before_j, temp_switch_j, temp_active_j, temp_after_j ];
