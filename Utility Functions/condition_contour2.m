5eb1912ee08474b6284ad0c4a8d869c3a9fe12e8
Hari Krishna Hari Prasad <hari.hariprasad@colorado.edu>
2024-10-19 17:17:10 -0600

    updates to two-foot gait ctrb and reach

--- Utility Functions/condition_contour2.m
+++ Utility Functions/condition_contour2.m
@@ -1,9 +1,10 @@
-function tempp = condition_contour2(temp, iter_old)
+function matOut = condition_contour2(temp, iter_old)
 %CONDITION_CONTOUR conditions an input contour matrix
-%   Sometimes the contour matrices generated for a single level splits it up over sub-arrays-- the goal is to number each contour result.
+%   Sometimes the contour matrices generated for a single level splits it 
+%   up over sub-arrays-- the goal is to number each contour result.
     
     % Initialize the return array, a counter, and a point count container (based on an input or fresh start)
-    tempp = []; count = size(temp, 2); N = 0;
+    matOut = []; count = size(temp, 2); N = 0;
     switch nargin
         case 2
             iter = iter_old;
@@ -23,7 +24,7 @@ function tempp = condition_contour2(temp, iter_old)
         % get the count
         N = N + temp(2,1);
         % update the return array
-        tempp = [ tempp, [iter*ones(1, temp(2,1)); temp( :, 2:temp(2,1)+1 )] ];
+        matOut = [ matOut, [iter*ones(1, temp(2,1)); temp( :, 2:temp(2,1)+1 )] ];
         % updated the counter and input array
         temp = temp(:, temp(2,1)+2:end);
         % update the new size
@@ -31,7 +32,7 @@ function tempp = condition_contour2(temp, iter_old)
     end
     
     % add the z-value and total contour points N as the first column: [z; N]
-    tempp = [ [nan; z; N], tempp ];
+    matOut = [ [nan; z; N], matOut ];
     
 end
 
