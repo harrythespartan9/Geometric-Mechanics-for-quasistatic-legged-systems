eedc6f59fc5f6d76927b5c3e96bf7af1b2afc4e3
Hari Krishna Hari Prasad <hari.hariprasad@colorado.edu>
2024-11-08 16:55:53 -0700

    updates to slip coordinate recognition functions
    1. improved the point along a line computing function, returns vectorial values
    2. fixed the algorithm for obtaining the new slipping axis

--- Utility Functions/returnPtAlongLineFromIC.m
+++ Utility Functions/returnPtAlongLineFromIC.m
@@ -1,9 +1,22 @@
 function pReq = returnPtAlongLineFromIC(pIC, unitPhasor, a)
 %RETURNPTALONGLINEFROMIC return a point along a line
-%   Given the line IC and a phasor multiplier, find a point along the line
+%   Given the line IC and a phasor multiplier, find a point along the line,
+%   or equivalently, given a list of a values, find all the points they
+%   correspond to along a line.
 
     % return the point as a column vector
-    pReq = pIC(:) + a*unitPhasor;
+    switch numel(a)
+        case 1
+            pReq = pIC(:)+ a*unitPhasor(:);
+        otherwise
+            numPts = size(a, 2);
+            if numPts < 2
+                a = a'; numPts = size(a, 2); % condition
+            end
+            pReq = repmat(pIC(:), [1, numPts]) + ...
+                repmat(a, [2, 1]).*...
+                repmat(unitPhasor(:), [1, numPts]); % vectorially compute 
+    end
 
 end
 
